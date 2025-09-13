97eb2608726f9d511f775d3ca19ade5c6f65b48b
Dimonoid2013@yandex.ru <Dimonoid2013@yandex.ru>
2025-09-13 12:30:04 +0300

    Add .gitignore and exclude VS artifacts

---
+++ GrpcServices/CustomerService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace OnlineStore.CustomerService.GrpcServices
+{
+    public sealed class CustomerService : Customers.CustomersBase
+    {
+        public override Task<GetCustomerByIdResponse> GetCustomerById(GetCustomerByIdRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetCustomerByIdResponse
+            {
+                Customer = new Customer
+                {
+                    Id = 1,
+                }
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,14 @@
-
-namespace OnlineStore
+namespace OnlineStore.CustomerService
 {
-    public class Program
+    public sealed class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            await Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+                .Build()
+                .RunAsync();
         }
     }
 }
